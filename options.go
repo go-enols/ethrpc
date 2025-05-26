9561d94907fcbc65e45d5a91e7336a55419eadff
go-enols <2575169674@qq.com>
2025-05-27 05:39:32 +0800

    feat(proxy): 添加 WithHttpProxy 函数以支持自定义 HTTP 代理

--- options.go
+++ options.go
@@ -3,6 +3,7 @@ package ethrpc
 import (
 	"io"
 	"net/http"
+	"net/url"
 )
 
 type httpClient interface {
@@ -16,6 +17,27 @@ type logger interface {
 // WithHttpClient set custom http client
 func WithHttpClient(client httpClient) func(rpc *EthRPC) {
 	return func(rpc *EthRPC) {
+		rpc.client = client
+	}
+}
+
+func WithHttpProxy(proxy string) func(rpc *EthRPC) {
+	return func(rpc *EthRPC) {
+		// 代理地址（如 Clash、v2ray 等默认 7890 端口）
+		proxyURL, err := url.Parse(proxy)
+		if err != nil {
+			panic(err)
+		}
+
+		// 创建自定义 Transport
+		transport := &http.Transport{
+			Proxy: http.ProxyURL(proxyURL), // 设置代理
+		}
+
+		// 创建 Client
+		client := &http.Client{
+			Transport: transport, // 使用自定义 Transport
+		}
 		rpc.client = client
 	}
 }
